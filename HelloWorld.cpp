1e1ecb6af2534c3b3ad34fbf0e6ce79e2a4c6d51
Ubuntu <ubuntu@ip-172-31-56-74.ec2.internal>
2020-11-23 18:57:36 +0000

    proper code style

---
+++ HelloWorld.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2020 Roman Cernetchi All rights reserved
+//
+// Created by: Roman Cernetchi
+// Created on: November 2020
+// This is a "Hello, World!" program, but it looks nicer
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+}
